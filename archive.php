83048ced960e52cf6cf2c8679d3f3cc80f1483a6
codwerk <louisws@outlook.fr>
2021-10-30 23:19:42 +0200

    page liste articles

---
+++ archive.php
@@ -0,0 +1,29 @@
+<?= 
+  get_header();
+?>
+<section class="page-wrap">
+  <div class="container">
+    <div class="row">
+      <!-- param 1: ou il se trouve, param 2: deuxième partie du nom  -->
+      <?= get_template_part('includes/section','archive'); ?>
+
+      
+      <div class="py-2">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <ul class="pagination" >
+                
+              <li class="page-item"> <?php previous_posts_link()?></li>
+              <li class="page-item"> <?php next_posts_link();?></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?=
+  get_footer();
+?>
